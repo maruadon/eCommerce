8bc42c271dadd08d4b66d8bccd6dab0749237234
unknown <maruf.ruet15@gmail.com>
2022-02-02 00:49:34 +0600

    first version of project complete

--- js/app.js
+++ js/app.js
@@ -3,9 +3,20 @@ $(document).ready(function () {
       arrows: false,
       dots: true,
       appendDots:'.slider_dots',
+      // slider_dots class e paste kore dilo plugin er code
       fade:true
       
     });  
+
+    $('.slider2').slick({
+      arrows: false,
+      dots:true,
+      slidesToShow:3,
+      slidesToscroll:1,
+      centerMode:true,
+      autoplay:true,
+      appendDots:'.slider2-dots', 
+    }); 
     
 });
 
